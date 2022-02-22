4828101cfe742a428434e42fcc0969db8309b614
andreaspi <andresun@stud.ntnu.no>
2022-02-22 14:02:08 +0100

    Lagt inn mye kommentarer for å forklare oppbyggingen av koden

--- SocketsClient.c
+++ SocketsClient.c
@@ -1,36 +1,50 @@
-   #include <stdio.h>
-   #include <sys/types.h>
-   #include <sys/socket.h>
-   #include <netinet/in.h>
-   #include <netdb.h> 
+#include <stdio.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> //defines the structure hostent
+   
+void error (char *msg)
+{
+        perror(msg);   
+        exit(0);
+}
 
-   void error(char *msg)
-   {
-       perror(msg);
-       exit(0);
-   }
+// har mer utfyllende kommentarer i serverkoden. Ikke vits å kommentere dobbelt opp på ting som er like
 
-   int main(int argc, char *argv[])
-   {
+int main(int argc, char *argv[])
+{
      int sockfd, portno, n;
-     struct sockaddr_in serv_addr;
-     struct hostent *server;
+     struct sockaddr_in serv_addr; 
+     struct hostent *server; // lager variablen server som en pointer til en structure av type hostent
+     // hostent har følgende felt:
+          // *h_name : navnet på hosten
+          // **h_aliases : en liste av alternative navn på hosten
+          // h_addr : addressetype. som regel AF_INET altså IPv4
+          // h_length : length in bytes of the address
+          // **h_addr_list, a pointer to a list of netwrok addresses for the named host
+     
      char buffer[256];
      if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
+          fprintf(stderr,"usage %s hostname port\n", argv[0]);
+          exit(o);
      }
      portno = atoi(argv[2]);
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
          error("ERROR opening socket");
-     server = gethostbyname(argv[1]);
+         
+     server = gethostbyname(argv[1]); // forsøker å hente hostent structure fra navnet til serveren som defineres i kallet på client
+     // funksjonen *gethostbyname(char, *name) er definert i netdb.h til å returnere en struct hostent. Returner altså en pointer til en hostent structure for the named host.
+     // hvis hostnavnet ikke er kjent lokalt bruker den DNS
      if (server == NULL) {
          fprintf(stderr,"ERROR, no such host\n");
          exit(0);
      }
      bzero((char *) &serv_addr, sizeof(serv_addr));
      serv_addr.sin_family = AF_INET;
+     
+     // siden hostent->h_addr er en character string. brukes bcopy(char *s1, char* s2, int length) til å kopiere length bytes fra s1 til s2
      bcopy((char *)server->h_addr, 
           (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
@@ -39,15 +53,15 @@
          error("ERROR connecting");
      printf("Please enter the message: ");
      bzero(buffer,256);
-     fgets(buffer,255,stdin);
-     n = write(sockfd,buffer,strlen(buffer));
+     fgets(buffer,255,stdin); // fgets leser melding fra stdin altså terminal, og putter verdiene i buffer
+     n = write(sockfd,buffer,strlen(buffer)); // skriver meldingen på socketen til serveren-
      if (n < 0) 
           error("ERROR writing to socket");
      bzero(buffer,256);
-     n = read(sockfd,buffer,255);
+     n = read(sockfd,buffer,255); // leser svarmelding
      if (n < 0) 
           error("ERROR reading from socket");
      printf("%s\n",buffer);
      return 0;
-   }
+}
 
--- SocketsServer.c
+++ SocketsServer.c
@@ -1,4 +1,5 @@
   #include <stdio.h>
+  #include <stdlib.h>
   #include <sys/types.h> 
   #include <sys/socket.h>
   #include <netinet/in.h>
@@ -9,39 +10,60 @@
       exit(1);
   }
 
-   int main(int argc, char *argv[])
-   {
+int main(int argc, char *argv[])
+{
         int sockfd, newsockfd, portno, clilen;
-        char buffer[256];
+        // sockfd stores value returned by socket system call
+        // newsockfd stores value returned by accept system call
+        // portno is port number
+        // clilen is size of client address
+        char buffer[256]; // The server reads characters from socket connection into this
         struct sockaddr_in serv_addr, cli_addr;
-        int n;
-        if (argc < 2) {
+        // defines server and client address structures using the sock_addr_in internet address structure
+        int n; // number of characters read or written by read() and write()
+        
+        if (argc < 2) { 
             fprintf(stderr,"ERROR, no port provided\n");
             exit(1);
         }
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        
+        sockfd = socket(AF_INET, SOCK_STREAM, 0); // blir socket(IPv4, StreamSocket, TCP)
         if (sockfd < 0) 
            error("ERROR opening socket");
+           
         bzero((char *) &serv_addr, sizeof(serv_addr));
-        portno = atoi(argv[1]);
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(portno);
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-        if (bind(sockfd, (struct sockaddr *) &serv_addr,
+        // bzero sets all values in a buffer to zero. Takes two arugment, pointer to the buffer and size of buffer
+        // altså intialisere den serv_addr til 0
+        
+        portno = atoi(argv[1]); // retrieves portnumber used to call program. atoi() converts from string of digits to integer
+        
+        //assigns values to serv_addr
+        serv_addr.sin_family = AF_INET; // IPv4
+        serv_addr.sin_port = htons(portno); // htons() converts port no from host byte order to network byte order. Host og network bytes bruker forskjellig rekkefølge i minnet
+        serv_addr.sin_addr.s_addr = INADDR_ANY; // Henter IP-addressen for serveren fra symbolic constant IN_ADDR_ANY
+        
+        // binds socket to socket address
+        if (bind(sockfd, (struct sockaddr *) &serv_addr, // må typecaste &serv_addr fra type sockaddr til type sockaddr_in
                  sizeof(serv_addr)) < 0) 
-                 error("ERROR on binding");
-        listen(sockfd,5);
-        clilen = sizeof(cli_addr);
-        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+                 error("ERROR on binding"); // throws error if result from bind is < 0 altså failed
+        
+        listen(sockfd,5); // begynner å høre på socketen. 5 er typisk verdi for backlog queue
+        
+        clilen = sizeof(cli_addr) ; // oppbevarer størrelsen på clientaddress
+        
+        // blokkerer prosessen til connection har blitt etablert med en server
+        // returnerer ny file descriptor newsockfd, og alle kommunikasjon på forbindelsen bør gjøres på newsockfd fra nå
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); 
         if (newsockfd < 0) 
              error("ERROR on accept");
-        bzero(buffer,256);
-        n = read(newsockfd,buffer,255);
+             
+        bzero(buffer,256); // initialiserer bufferen til 0
+        n = read(newsockfd,buffer,255); // leser opp til 255 bytes på bufferen fra newsockfd. returnerer antall characters lest til n. read() blokkerer i likhet med accept() prosessen til det er noe å lese
         if (n < 0) error("ERROR reading from socket");
-        printf("Here is the message: %s\n",buffer);
-        n = write(newsockfd,"I got your message",18);
+        printf("Here is the message: %s\n",buffer); // printer fra bufferen
+        n = write(newsockfd,"I got your message",18); // skriver melding på forbindelsen til klienten
         if (n < 0) error("ERROR writing to socket");
         return 0; 
-   }
+}
 
 
